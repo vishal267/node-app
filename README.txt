e05c9cb53d22af5ecc0cd588f8da2f58d46339f2
vishal <vishal1.arora@paytm.com>
2019-09-02 12:18:01 +0530

    changes related to ansible

---
+++ README.txt
@@ -0,0 +1,6 @@
+Deployment pipeline will  deploy 
+
+chmod +x multistage.sh
+bash multistage.sh  
+
+#It will deploy code in  dev qa and ua environment using ansible playbook and roles
--- multistage.sh
+++ multistage.sh
@@ -1,12 +1,12 @@
 #Dev Environment 
-ansible-playbook  node.yml -i deploy/hosts --private-key  /home/vishalarora/Downloads/sept.pem  -vvv -t nodejs
+ansible-playbook  node.yml -i deploy/hosts --private-key  /home/vishalarora/Downloads/sept.pem -e env=dev  -vvv -t nodejs
 
 
 #QA Environment 
 
-ansible-playbook  node.yml -i deploy/hosts --private-key  /home/vishalarora/Downloads/sept.pem  -vvv -t nodejs 
+ansible-playbook  node.yml -i deploy/hosts --private-key  /home/vishalarora/Downloads/sept.pem -e env=qa  -vvv -t nodejs 
 
 
 #UA Environment 
 
-ansible-playbook  node.yml -i deploy/hosts --private-key  /home/vishalarora/Downloads/sept.pem  -vvv -t nodejs
+ansible-playbook  node.yml -i deploy/hosts --private-key  /home/vishalarora/Downloads/sept.pem  -e env=ua -vvv -t nodejs
